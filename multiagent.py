2f9a2f0d98730f05c93c7f004eea34c605662f43
YashSax <yash.saxena@gmail.com>
2023-12-01 02:14:46 -0600

    started working on multiagent

---
+++ multiagent.py
@@ -0,0 +1,43 @@
+import ray
+from ray import tune
+from ray.rllib.env.multi_agent_env import MultiAgentEnv
+from utils import Ball, Agent, MultiAgentEnvironment
+import math
+
+ball = Ball(10, 0)
+agent1 = Agent(-20, ball, 1, damping=1)
+agent2 = Agent(5, ball, 1, damping=1)
+
+desired_trajectory = [10 * math.sin(i / 20) for i in range(0, 200)]
+
+env = MultiAgentEnvironment(ball, desired_trajectory, agent1, agent2, debug=False)
+
+# Initialize Ray
+ray.init()
+
+# Configuration and Setup for RLlib Trainer
+config = {
+    "env": env,
+    # Specify your desired RL algorithm here, e.g., "PPO", "DQN", etc.
+    "multiagent": {
+        "policies": {
+            # Define policies for each agent here if necessary
+            "agent1": (None, env.observation_space, env.action_space, {}),
+            "agent2": (None, env.observation_space, env.action_space, {}),
+            # Add more agents if needed
+        },
+        "policy_mapping_fn": lambda agent_id: agent_id,  # Simplest mapping
+    },
+    # Other hyperparameters and configurations for the chosen algorithm
+}
+
+# Train the agents using RLlib
+analysis = tune.run(
+    "PPO",  # Replace with your desired algorithm
+    config=config,
+    stop={"training_iteration": 100},  # Define stopping criteria
+    checkpoint_at_end=True  # Save a checkpoint at the end of training
+)
+
+# Shutdown Ray when done
+ray.shutdown()
--- utils.py
+++ utils.py
@@ -218,3 +218,140 @@ class Environment(gym.Env):
             self.screen, (0, 0, 0), (desired_adjusted, self.height / 2), 5
         )
         pygame.display.update()
+
+
+class MultiAgentEnvironment(gym.Env):
+    def __init__(self, ball, desired_trajectory, agent1, agent2, debug=False):
+        super(MultiAgentEnvironment, self).__init__()
+        self.ball = ball
+        self.agent1 = agent1
+        self.agent2 = agent2
+        self.desired_trajectory = desired_trajectory
+        self.curr_timestep = 0
+        self.debug = debug
+        self.cumulative_reward = 0
+
+        obs_low = [-float("inf")] * 2
+        obs_high = [float("inf")] * 2
+        self.observation_space = Box(
+            low=np.array(obs_low), high=np.array(obs_high), dtype=np.float32
+        )
+
+        agent1_high, agent1_low = agent1.max_step_distance, -agent1.max_step_distance
+        agent2_high, agent2_low = agent2.max_step_distance, -agent2.max_step_distance
+        action_highs = [agent1_high]
+        action_lows = [agent1_low]
+        self.action_space = Box(
+            low=np.array(action_lows), high=np.array(action_highs), dtype=np.float32
+        )
+
+        if debug:
+            pygame.init()
+            self.width = 800
+            self.height = 600
+            self.screen = pygame.display.set_mode((self.width, self.height))
+
+    def get_reward(self):
+        reward = -1 * abs(
+            self.desired_trajectory[self.curr_timestep] - self.ball.position
+        )
+        return reward
+
+    def reset(self, seed=1):
+        self.curr_timestep = 0
+        self.ball.reset()
+        self.agent1.reset()
+        self.agent2.reset()
+
+        a1_obs = np.array(
+            [self.desired_trajectory[self.curr_timestep] - self.ball.position, self.ball.velocity]
+        )
+
+        return a1_obs.reshape((1, -1)), {}
+
+    def __repr__(self):
+        return f"Ball = {self.ball.position}, agent1 = {self.agent1.position}, agent2 = {self.agent2.position}"
+
+    def get_state(self):
+        return GameState(
+            timestep=self.curr_timestep,
+            ball_position=self.ball.position,
+            ball_velocity=self.ball.velocity,
+            agent1_position=self.agent1.position,
+            agent2_position=self.agent2.position,
+            desired_position=self.desired_trajectory[self.curr_timestep]
+        )
+
+    def load_state(self, state):
+        self.curr_timestep = state.timestep
+        self.ball.position = state.ball_position
+        self.ball.velocity = state.ball_velocity
+        self.agent1.position = state.agent1_position
+        self.agent2.position = state.agent2_position
+
+    def step(self, agent_moves):
+        agent1_move, agent2_move = agent_moves
+        # Returns (observation, reward, done)
+        if type(agent1_move) is np.ndarray:
+            agent1_move = agent1_move.item(0)
+        if type(agent2_move) is np.ndarray:
+            agent2_move = agent2_move.item(0)
+
+        self.agent1.move(agent1_move)
+        self.agent2.move(agent2_move)
+
+        total_force = self.agent1.get_force_applied() + self.agent2.get_force_applied()
+        self.ball.move(total_force)
+
+        desired_position = self.desired_trajectory[self.curr_timestep]
+        a1_obs = np.array([desired_position - self.ball.position, self.ball.velocity])
+        reward = -1 * abs(self.ball.position - desired_position)
+        self.cumulative_reward += reward
+        self.curr_timestep += 1
+        return (
+            a1_obs.reshape(1, -1),
+            reward,
+            self.curr_timestep >= len(self.desired_trajectory) - 1,
+            False,
+            {}
+        )
+
+    def render(self):
+        assert (
+            self.debug
+        ), "Cannot render if Environment was initialized with debug=False"
+        time.sleep(0.1)
+        self.screen.fill((255, 255, 255))
+
+        agent1_adjusted = self.width / 2 + 10 * self.agent1.position
+        agent2_adjusted = self.width / 2 + 10 * self.agent2.position
+        ball_adjusted = self.width / 2 + 10 * self.ball.position
+        desired_adjusted = (
+            self.width / 2 + 10 * self.desired_trajectory[self.curr_timestep]
+        )
+
+        pygame.draw.line(
+            self.screen,
+            (127, 0, 127),
+            (agent1_adjusted, self.height / 2),
+            (ball_adjusted, self.height / 2),
+        )
+        pygame.draw.line(
+            self.screen,
+            (0, 127, 0),
+            (agent2_adjusted, self.height / 2),
+            (ball_adjusted, self.height / 2),
+        )
+        pygame.draw.circle(
+            self.screen, (255, 0, 0), (agent1_adjusted, self.height / 2), 15
+        )
+        pygame.draw.circle(
+            self.screen, (255, 0, 0), (agent2_adjusted, self.height / 2), 15
+        )
+        pygame.draw.circle(
+            self.screen, (0, 255, 0), (ball_adjusted, self.height / 2), 5
+        )
+        pygame.draw.circle(
+            self.screen, (0, 0, 0), (desired_adjusted, self.height / 2), 5
+        )
+        pygame.display.update()
